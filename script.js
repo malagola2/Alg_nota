2fda63285df5ff5d64c00189649f89d62d10f1d0
RAFAEL SARAGOCA MALAGOLA <rafael.smalagola@senacsp.edu.br>
2024-09-25 14:51:51 -0300

    Algoritimo de verificação de nota

---
+++ script.js
@@ -0,0 +1,9 @@
+// Solicitar a nota do aluno
+
+let nota = parseInt(prompt("Insira sua Nota"));
+
+// Verificar se a nota é maior que 7
+
+if (nota >= 7) {
+    alert ("Aluno Aprovado! ");
+}else{alert("Aluno Reprovado")}
